aeaff4c8ec84dd071d09876bb168f4e64a85ab8d
yasin7a <arafath7yasin@gmail.com>
2022-09-10 19:53:26 +0600

    v20

--- pages/products/photography/[slug].js
+++ pages/products/photography/[slug].js
@@ -1,88 +1,95 @@
-import Main from "../../../components/each-profile/Main"
-import * as queries from '../../../src/graphql/queries'
-import { API,withSSRContext } from "aws-amplify";
-import { useRouter } from 'next/router'
+import Main from "../../../components/each-profile/Main";
+import * as queries from "../../../src/graphql/queries";
+import { API, withSSRContext } from "aws-amplify";
+import { useRouter } from "next/router";
 import services from "../../../utils/services";
 import Head from "next/head";
-function ViewPhotography({posts,rating,sLocation,specializedIn}){
-  const data = posts?.serviceLocation == null ?  "": posts;
-    return(
- <>
-     <Head>
-    <title>{posts?.title}</title>
-    <meta property="og:title" content={posts?.title} key="title" />
-    <meta name="description" content={posts?.detailsAboutYou}/>
-    <meta property="og:url" content={"https://myeventizer.com/products/photography/"+posts?.id} />
-    </Head>
-    <Main data={data} service={services.photography} rating={rating} sLocation={sLocation} specializedIn={specializedIn}/>
- </>
-    )
+function ViewPhotography({ posts, rating, sLocation, specializedIn }) {
+  const data = posts?.serviceLocation == null ? "" : posts;
+  return (
+    <>
+      <Head>
+        <title>{posts?.title}</title>
+        <meta property="og:title" content={posts?.title} key="ogtitle" />
+        <meta name="twitter:title" content={posts?.title} />
+        <meta
+          property="og:url"
+          content={"https://myeventizer.com/products/photography/" + posts?.id}
+        />
+        <meta property="og:description" content={posts?.detailsAboutYou} />
+        <meta name="twitter:description" content={posts?.detailsAboutYou} />
+      </Head>
+
+      <Main
+        data={data}
+        service={services.photography}
+        rating={rating}
+        sLocation={sLocation}
+        specializedIn={specializedIn}
+      />
+    </>
+  );
 }
 
-export async function getStaticProps({params}) {
-
-    const res =  await API.graphql({
-      query: queries.getPhotography,
-      variables: { id: params.slug },
-      authMode: 'API_KEY',
-      
-    });
-    const posts = await res?.data?.getPhotography
-    let s =""
-    let c=""
-    let k = posts?.Reviews?.items?.length
-    let r=0
-    const sum = posts?.Reviews?.items?.reduce((accumulator, object) => {
-      return accumulator + object.average;
-    }, 0);
-    r =sum/k ||0
-    posts?.serviceLocation?.map((e)=>{
-      let d =JSON.parse(e)
-      if(s.length===0){
-        s=d?.label
-      }
-      else{
-        s=s+", "+d?.label
-      }
-    })
-    posts?.specializedIn?.map((e)=>{
-      let m =JSON.parse(e)
-      if(c.length===0){
-        c=m?.label
-      }
-      else{
-        c=c+", "+m?.label
-      }
-
-    })
+export async function getStaticProps({ params }) {
+  const res = await API.graphql({
+    query: queries.getPhotography,
+    variables: { id: params.slug },
+    authMode: "API_KEY",
+  });
+  const posts = await res?.data?.getPhotography;
+  let s = "";
+  let c = "";
+  let k = posts?.Reviews?.items?.length;
+  let r = 0;
+  const sum = posts?.Reviews?.items?.reduce((accumulator, object) => {
+    return accumulator + object.average;
+  }, 0);
+  r = sum / k || 0;
+  posts?.serviceLocation?.map((e) => {
+    let d = JSON.parse(e);
+    if (s.length === 0) {
+      s = d?.label;
+    } else {
+      s = s + ", " + d?.label;
+    }
+  });
+  posts?.specializedIn?.map((e) => {
+    let m = JSON.parse(e);
+    if (c.length === 0) {
+      c = m?.label;
+    } else {
+      c = c + ", " + m?.label;
+    }
+  });
   return {
     props: {
-      posts:posts,
-      rating:parseFloat(r.toFixed(2)),
-      sLocation:s,
-      specializedIn:c
+      posts: posts,
+      rating: parseFloat(r.toFixed(2)),
+      sLocation: s,
+      specializedIn: c,
     },
     revalidate: 10, // In seconds
-  }
+  };
 }
 
 export async function getStaticPaths() {
   const SSR = withSSRContext();
   let filter = {
-    status: { eq: "Accepted" } 
-};
+    status: { eq: "Accepted" },
+  };
   const res = await SSR.API.graphql({
     query: queries.listPhotographies,
-    authMode: 'API_KEY',
-    variables: { filter: filter}
+    authMode: "API_KEY",
+    variables: { filter: filter },
   });
 
-    const photographer = await res?.data?.listPhotographies?.items
-    const paths = photographer.map((e) => ({
-      params: { slug: e.id },
-    }))
+  const photographer = await res?.data?.listPhotographies?.items;
+  const paths = photographer.map((e) => ({
+    params: { slug: e.id },
+  }));
 
-    return { paths, fallback: 'blocking' }
-  }
+  return { paths, fallback: "blocking" };
+}
 
-export default ViewPhotography
+export default ViewPhotography;
