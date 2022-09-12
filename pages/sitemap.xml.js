4f578da983f54be02d1637f85b3843ab30a7c3a8
TanvirXen <mail@tanvirishtiaq.com>
2022-09-12 21:38:34 +0600

    Testing

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,52 @@
+import React from "react";
+import fs from "fs";
+
+const Sitemap = () => {};
+const dat ={}
+export const getServerSideProps = ({ res }) => {
+  const baseUrl ="https://myeventizer.com";
+
+  const staticPages = fs
+    .readdirSync({
+        development: 'pages',
+        production: './.next/server/pages',
+      }[process.env.NODE_ENV])
+    .filter((staticPage) => {
+      return ![
+        "_app.js",
+        "_document.js",
+        "_error.js",
+        "sitemap.xml.js",
+      ].includes(staticPage);
+    })
+    .map((staticPagePath) => {
+      return `${baseUrl}/${staticPagePath}`;
+    });
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${staticPages
+        .map((url) => {
+          return `
+            <url>
+              <loc>${url}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod>
+              <changefreq>monthly</changefreq>
+              <priority>1.0</priority>
+            </url>
+          `;
+        })
+        .join("")}
+    </urlset>
+  `;
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default Sitemap;
