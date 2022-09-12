a8b0e54694c1b3787bb3a47186817f09f7a08f22
TanvirXen <mail@tanvirishtiaq.com>
2022-09-12 22:43:11 +0600

    Test

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -11,7 +11,7 @@ export const getServerSideProps = async ({ res }) => {
   const staticPages =  fs
   .readdirSync({
     development: 'pages',
-    production: './.next/',
+    production: './.next/server',
   }[process.env.NODE_ENV])
     .filter((staticPage) => {
       return ![
