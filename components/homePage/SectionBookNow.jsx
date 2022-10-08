ea7c2849554dd3c23929805e9fd3839a7e68d99c
protimsaha <protimsaha750@gmail.com>
2022-10-09 01:37:17 +0600

    padding top added

--- components/homePage/SectionBookNow.jsx
+++ components/homePage/SectionBookNow.jsx
@@ -14,7 +14,7 @@ const SectionBusiness = () => {
       <div className=" container py-8 bg-FAFAFA">
         <div className="flex-1 md:flex items-center   ">
           <img src="/img/corporate.png" className="w-full md:w-2/5 rounded-lg " alt='img' />
-          <div className='pl-0 md:pl-8'>
+          <div className='pl-0 md:pl-8 pt-4 md:pt-0'>
             <h1 className="text-5xl font-bold">Book a free event consultant </h1>
             <p className="py-6">For your forthcoming events, it is better to talk with an event professional. Hire an event planner to help you arrange a successful event.</p>
             <ButtonLinkOrClick
